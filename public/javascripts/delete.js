d03a93f157b0e10a9d9722ad0b2d100e3cf7eb18
Bhanushali <zsypg7@splm.siemens.com>
2021-04-26 12:43:59 +0530

    Search done

--- public/javascripts/delete.js
+++ public/javascripts/delete.js
@@ -1,3 +1,9 @@
+document.getElementById("delete").onclick = function (evt) {
+  evt.preventDefault();
+  const productId = document.getElementById("product-id").value;
+  axios.delete(`/api/products/${productId}`).then(processResults);
+};
+
 function processResult() {
   window.alert("Product deleted!");
 }
--- routes/products.js
+++ routes/products.js
@@ -1,34 +1,79 @@
 var express = require("express");
 var router = express.Router();
+const qs = require('qs');
 
 module.exports = function (db) {
   router.get("/products", (req, res) => {
-    res.send(db.get("products").value());
-  });
-
-  router.post("/products", (req, res) => {
-    const newProduct = req.body;
-    res.send(db.get("products").insert(newProduct).write());
-  });
+      res.send(db.get("products").value());
+    });
+    
+    router.post("/products", (req, res) => {
+      const newProduct = req.body;
+      res.send(db.get("products").insert(newProduct).write());
+    });
 
   router.patch("/products/:id", (req, res) => {
     res.send(db.get("products").find({ id: req.params.id }).assign(req.body).write());
   });
 
   router.delete("/products/:id", (req, res) => {
-    db.get("products").remove({id: req.params.id}).write();
+    db.get("products").remove({ id: req.params.id }).write();
     res.status(204).send();
   });
 
+  router.route('/products/search').get((req, res) => {
+
+    const keywords = req.query.keywords;
+    const result = db.get("products").filter((_) => {
+      const fullText = _.description + _.name + _.color;
+      return fullText.indexOf(keywords) !== -1;
+    });
+
+    res.send(result);
+  });
+
+  router.route('/products/detailSearch').get((req, res) => {
+
+    const query = qs.parse(req.query);
+    const result = db.get("products").filter((_) => {
+      
+      return Object.keys(query).reduce((found, key) => {
+
+        const obj = query[key];
+
+        switch(obj.op){
+          case "lt":
+            found = found && _[key] < obj.val;
+            break;
+          
+            case "eq":
+              found = found && _[key] == obj.val;
+              break;
+
+            default:
+              found = found && _[key].indexOf(obj.val) !== -1;
+              break;
+        }
+        
+        return found;
+      }, true)
+    });
+
+    res.send(result);
+  });
+
+
   router.get("/products/:id", (req, res) => {
     const result = db.get("products").find({ id: req.params.id }).value();
-    if(result){
+    if (result) {
       res.send(result);
-    } else{
+    } else {
       res.status(404).send();
     }
 
   });
+
+
 
   return router;
 };
